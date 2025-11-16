58544f3d309fe20663d2c4fdfca1a8dc0ee46d23
Ikrame Baskane <baskaneikrame1@gmail.com>
2025-11-16 19:42:57 +0100

    Fin de Sprint3

--- backend/src/main/java/com/marketplace/catalog/entity/Produit.java
+++ backend/src/main/java/com/marketplace/catalog/entity/Produit.java
@@ -6,6 +6,7 @@ import com.marketplace.interaction.entity.Interaction;
 import com.marketplace.user.entity.Client;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -53,7 +54,7 @@ public class Produit {
     private Categorie categorie;
 
     @OneToMany(mappedBy = "produit", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<ProduitImage> images;
+    private List<ProduitImage> images=new ArrayList<>();;
 
     @OneToMany(mappedBy = "produit", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Interaction> interactions;
@@ -108,7 +109,19 @@ public class Produit {
     public void setCategorie(Categorie categorie) { this.categorie = categorie; }
 
     public List<ProduitImage> getImages() { return images; }
-    public void setImages(List<ProduitImage> images) { this.images = images; }
+    public void setImages(List<ProduitImage> images) {
+        if (this.images == null) {
+            this.images = new ArrayList<>();
+        }
+        this.images.clear(); // On vide la collection existante
+        if (images != null) {
+            for (ProduitImage image : images) {
+                image.setProduit(this); // IMPORTANT: établir la relation bidirectionnelle
+                this.images.add(image);
+            }
+        }
+    }
+
 
     public List<Interaction> getInteractions() { return interactions; }
     public void setInteractions(List<Interaction> interactions) { this.interactions = interactions; }
